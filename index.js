8d99e10caf269e52d3bf65f9e342a1583ad5bc38
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-03-23 17:10:59 +0100

    Dice Chalange

---
+++ index.js
@@ -0,0 +1,10 @@
+function refresh(){
+window.location.reload("Refresh");
+}
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDicePhoto = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDicePhoto);
+
+let randomNumber2 = Math.floor(Math.random() * 6) +1;
+let randomDicePhoto2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomDicePhoto2);
