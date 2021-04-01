3f0e9f3ad9697370270d17b7654b5025ac0686e3
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-01 16:08:28 +0200

    add footer

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
+
+
+
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomDicePhoto = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomDicePhoto);
@@ -8,5 +11,10 @@ let randomDicePhoto2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomDicePhoto2);
 function refresh(){
 window.location.reload("Refresh");
-
 }
+
+
+
+
+
+
