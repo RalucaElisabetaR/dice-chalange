9aa0b7b2156031f4b5929d934430984ec563fea6
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2021-04-02 17:25:44 +0200

    background color

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomDicePhoto = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", randomDicePhoto);
 
-let randomNumber2 = Math.floor(Math.random() * 6) +1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomDicePhoto2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomDicePhoto2);
-function refresh(){
-window.location.reload("Refresh");
+function refresh() {
+  window.location.reload("Refresh");
 }
 
 
